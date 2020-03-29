846022bbe096939d08481985e1fa1ae269ab172d
Ike Mawira <mawiraike@gmail.com>
2020-03-30 02:42:03 +0300

    Client code
    Signed-off-by: Ike Mawira <mawiraike@gmail.com>

--- src/cljs/daraja/core.cljs
+++ src/cljs/daraja/core.cljs
@@ -1,7 +1,14 @@
 (ns ^:figwheel-hooks daraja.core
   (:require
    [goog.dom :as gdom]
-   [reagent.core :as reagent :refer [atom]]))
+   [reagent.core :as reagent :refer [atom]]
+   [re-frame.core :as re-frame]
+   [cljs.core.async :as async  :refer (<! >! put! chan)]
+   [taoensso.encore :as encore :refer-macros (have have?)]
+   [taoensso.timbre :as timbre :refer-macros (tracef debugf infof warnf errorf)]
+   [taoensso.sente :as sente :refer (cb-success?)]
+   [taoensso.sente.packers.transit :as sente-transit]
+   ))
 
 (println "This text is printed from src/daraja/core.cljs. Go ahead and edit it and see reloading in action.")
 
@@ -17,7 +24,7 @@
 (defn hello-world []
   [:div
    [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/daraja/core.cljs and watch it change!"]])
+   [:h3 "Edit this in src/daraja/core.cljs and watch it change! Hope so!"]])
 
 (defn mount [el]
   (reagent/render-component [hello-world] el))
@@ -26,14 +33,73 @@
   (when-let [el (get-app-element)]
     (mount el)))
 
+
+
+(let [packer (sente-transit/get-transit-packer)
+      {:keys [chsk ch-recv send-fn state]}
+      (sente/make-channel-socket-client! "/chsk"
+                                         {:type :auto
+                                          :packer packer})]
+  (def chsk chsk)
+  (def ch-chsk ch-recv)
+  (def chsk-send! send-fn)
+  (def chsk-state state))
+
+(defmulti -event-msg-handler :id)
+
+(defn event-msg-handler [{:as ev-msg :keys [id ?data event]}]
+  (debugf "Event: %s" event) []
+  (-event-msg-handler ev-msg))
+
+(defmethod -event-msg-handler :default
+  [{:as ev-msg :keys [event]}]
+  (debugf "Unhandled event: %s" event))
+
+(defmethod -event-msg-handler :chsk/state
+  [{:as ev-msg :keys [?data]}]
+  (let [[old-state-map new-state-map] (have vector? ?data)]
+    (if (:first-open? new-state-map)
+      (debugf "Channel socket successfully established!: %s" ?data)
+      (debugf "Channel socket state change: %s" ?data))))
+
+(defmethod -event-msg-handler :chsk/handshake
+  [{:as ev-msg :keys [?data]}]
+  (let [[?uid ?csrf-token ?handshake-data] ?data]
+    (debugf "Handshake: %s" ?data)))
+
+
+
+;; This is the main event handler; If we want to do cool things with other kinds of data going back and forth,
+;; this is where we'll inject it.
+(defmethod -event-msg-handler :chsk/recv
+  [{:as ev-msg :keys [?data]}]
+  (let [[id msg] ?data]
+    (case id
+      :daraja.core/auth (swap! app-state assoc :view-spec msg)
+      (debugf "Push event from server: %s" ?data))))
+
+(def router_ (atom nil))
+
+(defn stop-router! []
+  (when-let [stop-f @router_] (stop-f)))
+
+(defn start-router! []
+  (stop-router!)
+  (reset! router_ (sente/start-client-chsk-router! ch-chsk event-msg-handler)))
+
+(defn start! []
+  (start-router!))
+
+
+
+
 ;; conditionally start your application based on the presence of an "app" element
 ;; this is particularly helpful for testing this ns without launching the app
 (mount-app-element)
 
-;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
   (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
+
+(start!)
