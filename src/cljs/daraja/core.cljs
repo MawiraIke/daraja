b60bd4c783636fe9ccccc2b10348f29a187d610f
Ike Mawira <mawiraike@gmail.com>
2020-03-31 04:45:29 +0300

    Code cleanup
    Signed-off-by: Ike Mawira <mawiraike@gmail.com>

--- src/cljs/daraja/core.cljs
+++ src/cljs/daraja/core.cljs
@@ -1,68 +1,49 @@
 (ns ^:figwheel-hooks daraja.core
   (:require
-   [goog.dom :as gdom]
-   [reagent.core :as reagent :refer [atom]]
-   [re-frame.core :as re-frame]
-   [cljs.core.async :as async  :refer (<! >! put! chan)]
-   [taoensso.encore :as encore :refer-macros (have have?)]
-   [taoensso.timbre :as timbre :refer-macros (tracef debugf infof warnf errorf)]
-   [taoensso.sente :as sente :refer (cb-success?)]
-   [taoensso.sente.packers.transit :as sente-transit]
-   ))
+    [reagent.core :as reagent :refer [atom]]
+    [cljs.core.async :refer (<! >! put! chan)]
+    [taoensso.encore :refer-macros (have have?)]
+    [taoensso.timbre :refer-macros (tracef debugf infof warnf errorf)]
+    [taoensso.sente :as sente :refer (cb-success?)]
+    [taoensso.sente.packers.transit :as sente-transit]
+    ))
 
 (println "This text is printed from src/daraja/core.cljs. Go ahead and edit it and see reloading in action.")
 
 (defn multiply [a b] (* a b))
 
-
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
-
-(defn get-app-element []
-  (gdom/getElement "app"))
-
-(defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/daraja/core.cljs and watch it change!"]])
-
-(defn mount [el]
-  (reagent/render-component [hello-world] el))
-
-(defn mount-app-element []
-  (when-let [el (get-app-element)]
-    (mount el)))
-
-;;;; Util for logging output to on-screen console
-
-(def output-el (.getElementById js/document "output"))
-(defn ->output! [fmt & args]
-  (let [msg (apply encore/format fmt args)]
-    (timbre/debug msg)
-    (aset output-el "value" (str "• " (.-value output-el) "\n" msg))
-    (aset output-el "scrollTop" (.-scrollHeight output-el))))
-
-(->output! "ClojureScript appears to have loaded correctly.")
-
-(def ?csrf-token
-  (when-let [el (.getElementById js/document "sente-csrf-token")]
-    (.getAttribute el "data-csrf-token")))
-
-(if ?csrf-token
-  (->output! "CSRF token detected in HTML, great!")
-  (->output! "CSRF token NOT detected in HTML, default Sente config will reject requests"))
-
-
 (let [packer (sente-transit/get-transit-packer)
       {:keys [chsk ch-recv send-fn state]}
       (sente/make-channel-socket-client! "/chsk"
-                                         ?csrf-token
-                                         {:type :auto
+                                         {:type   :auto
                                           :packer packer})]
   (def chsk chsk)
   (def ch-chsk ch-recv)
   (def chsk-send! send-fn)
   (def chsk-state state))
+
+
+;; define your app data so that it doesn't get over-written on reload
+(defonce app-state (atom {:text "Hello world!"}))
+
+
+(defn hello-world []
+  [:div
+   [:p (:text @app-state)]
+   [:p "Edit this in src/daraja/core.cljs and watch it change!"]
+   [:button
+    {:on-click (fn [e] (do (chsk-send! [:example/test-rapid-push])
+                         (js/console.log "Sending request !!")))}
+    "Try this"]
+   [:button
+    {:on-click (fn [e] (do (sente/chsk-disconnect! chsk)
+                         (js/console.log "Disconnecting !!")))}
+    "Disconnect"]
+   [:button
+    {:on-click (fn [e] (do (sente/chsk-reconnect! chsk)
+                         (js/console.log "Reconnecting !!")))}
+    "Reconnect"]])
+
 
 
 ;; Sente event handlers
@@ -72,33 +53,27 @@
 (defn event-msg-handler [{:as ev-msg :keys [id ?data event]}]
   (debugf "Event: %s" event) []
   (-event-msg-handler ev-msg))
-
 (defmethod -event-msg-handler :default
   [{:as ev-msg :keys [event]}]
   (debugf "Unhandled event: %s" event))
-
 (defmethod -event-msg-handler :chsk/state
   [{:as ev-msg :keys [?data]}]
   (let [[old-state-map new-state-map] (have vector? ?data)]
     (if (:first-open? new-state-map)
       (debugf "Channel socket successfully established!: %s" ?data)
       (debugf "Channel socket state change: %s" ?data))))
-
 (defmethod -event-msg-handler :chsk/handshake
   [{:as ev-msg :keys [?data]}]
   (let [[?uid ?csrf-token ?handshake-data] ?data]
     (debugf "Handshake: %s" ?data)))
-
-
-
-;; This is the main event handler; If we want to do cool things with other kinds of data going back and forth,
-;; this is where we'll inject it.
 (defmethod -event-msg-handler :chsk/recv
   [{:as ev-msg :keys [?data]}]
   (let [[id msg] ?data]
-    (case id
-      :daraja.core/auth (swap! app-state assoc :view-spec msg)
-      (debugf "Push event from server: %s" ?data))))
+    (cond (= id :daraja.core/:auth)
+          (swap! app-state assoc :new-msg msg)
+
+          :default
+          (debugf "Push event from server: %s" ?data))))
 
 ;; TODO Add (defmethod -event-msg-handler <event-id> [ev-msg] <body>)s here...
 
@@ -110,19 +85,17 @@
   (stop-router!)
   (reset! router_ (sente/start-client-chsk-router! ch-chsk event-msg-handler)))
 
-(defn start! []
-  (start-router!))
 
+(defn mount
+  []
+  (let [container (.getElementById js/document "app")]
+    (reagent/render-component  [hello-world] container)))
 
-
-
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
-(mount-app-element)
+(mount)
 
 (defn ^:after-load on-reload []
-  (mount-app-element)
+  (mount)
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
 
-(start!)
+(start-router!)
